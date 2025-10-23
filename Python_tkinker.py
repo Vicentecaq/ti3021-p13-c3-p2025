ae460af751bbd5caaae9b5b4284b5e46d60ffdf5
Vicentecaq <vicente.cayuqueo@inacapmail.cl>
2025-10-23 10:41:29 -0300

    Clase python_tkinter
    clase python tkinter

---
+++ Python_tkinker.py
@@ -0,0 +1,12 @@
+# OBLIGATORIO: Importar la libreria TKINTER
+from tkinter import *
+from tkinter import ttk
+
+# OBLIGATORIO: inicializar TKINTER
+root: Tk = Tk()
+
+# Crear lienzo
+frame1: Frame = ttk.Frame( root, padding=10 )
+
+# OBLIGATORIO: Mantener el ciclo de vida
+root.mainloop()
