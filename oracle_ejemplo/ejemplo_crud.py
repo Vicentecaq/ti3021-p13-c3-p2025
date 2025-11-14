66af0cddc08acffedaef222ba6024c7ef505325a
Deztiwanolita <jhonquirino14@gmail.com>
2025-11-14 11:16:32 -0300

    clase 14-11-25

--- oracle_ejemplo/ejemplo_crud.py
+++ oracle_ejemplo/ejemplo_crud.py
@@ -7,25 +7,19 @@ username = os.getenv("ORACLE_USER")
 dsn = os.getenv("ORACLE_DSN")
 password = os.getenv("ORACLE_PASSWORD")
 
-def get_connection():     
-    return oracledb.connect(user=username, password= password, dsn=dsn)
+def get_connection():
+    return oracledb.connect(user=username, password=password, dsn=dsn)
 
-def create_table_personas():
-    query = (
-        "CREATE TABLE personas ("
-        "rut VARCHAR2(50) PRIMARY KEY,"
-        "nombres VARCHAR2(200),"
-        "apellidos VARCHAR2(200),"
-        "fecha_nacimiento DATE,"
-        "cod_area VARCHAR2(20),"
-        "numero_telefono VARCHAR2(50)"
-        ")"
-    )
-    
+def create_schema(query):
     try:
         with get_connection() as connection:
             with connection.cursor() as cursor:
                 cursor.execute(query)
-                print("Tabla 'personas' creada.")
+                print(f"Tabla creada \n {query}")
     except oracledb.DatabaseError as error:
         print(f"No se pudo crear la tabla: {error}")
+
+tables = [
+]
+for query in tables:
+    create_schema(query)
